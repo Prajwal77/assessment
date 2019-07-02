7ea10d61fcd2ebf2f68dee435dfdd7f7b188f195
Shetty <shetty@macbook-pro.home>
2019-07-02 20:50:48 +0100

    Response will ignore calories if null

--- src/main/java/org/sainsbury/vo/Product.java
+++ src/main/java/org/sainsbury/vo/Product.java
@@ -1,11 +1,15 @@
 package org.sainsbury.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
 public class Product {
     private String title;
-    private int kcal_per_100g;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer kcal_per_100g;
+
     private float unit_price;
     private String description;
 }
